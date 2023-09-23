9780f05825781a4633c120a19d22d36e4d6d566f
abdukodir_soliev <91322310+asoliev@users.noreply.github.com>
2023-09-23 22:04:32 +0500

    refactor: phase 2
    radically changed the structure

--- audio-player/index.js
+++ audio-player/index.js
@@ -1,101 +1,144 @@
-const playlist = ['assets/audio/beyonce.mp3', 'assets/audio/dontstartnow.mp3'];
-const thumbnails = ['./assets/img/lemonade.png', './assets/img/dontstartnow.png'];//paths for album covers and backgrounds
-const songArtists = ['Beyonce', 'Dua Lipa'];//track artists
-const songTitles = ["Don't Hurt Yourself", "Don't Start Now"];//track titles
-
-const playPauseBtn = document.getElementById('playPause');
-const stopBtn = document.getElementById('stop');
-const previousBtn = document.getElementById('prev');
-const nextBtn = document.getElementById('next');
-const muteBtn = document.getElementById('mute');
-const progressBar = document.getElementById('progressBar');
-const currentTime = document.getElementById('currentTime');
-const durationTime = document.getElementById('durationTime');
-const volumeControl = document.getElementById('volumeControl');
-const songArtist = document.getElementById('trackArtist');
-const songTitle = document.getElementById('trackTitle');
-const playerBox = document.getElementById('playerBox');
-
-let playIndex = 0;
 const audio = new Audio();
-audio.src = playlist[playIndex];
-audio.volume = 0.5;
-volumeControl.value = audio.volume;
-let lastAudioVolume = audio.volume;
-songArtist.textContent = songArtists[playIndex];
-songTitle.textContent = songTitles[playIndex];
-playerBox.style.backgroundImage = `url('${thumbnails[playIndex]}')`;
 
-function timeFormat(seconds) {
-  let sec = `${seconds}`.padStart(2, "0");
+function mainActions() {
+  function volumeActions() {
+    const volumeControl = document.getElementById('volumeControl');
+    const muteBtn = document.getElementById('mute');
 
-  if (seconds < 10) return `0:${sec}`;
+    audio.volume = 0.25;
+    volumeControl.value = audio.volume;
+    volumeControl.title = audio.volume * 100;
 
-  const min = Math.floor(seconds / 60);
-  
-  sec = seconds - min * 60;
-  sec = `${sec}`.padStart(2, "0");
-  return `${min}:${sec}`;
-}
-function updateProgressValue() {
-  progressBar.value = audio.currentTime;
-  currentTime.textContent = timeFormat(Math.round(audio.currentTime));
+    volumeControl.addEventListener('change', () => {
+      audio.volume = volumeControl.value;
+      volumeControl.title = audio.volume * 100;
+    });
 
-  progressBar.max = audio.duration;
-  durationTime.textContent = timeFormat(Math.round(audio.duration));
-}
-setInterval(updateProgressValue, 1000);
+    muteBtn.addEventListener('click', () => {
+      muteBtn.firstElementChild.classList.toggle("hide");
+      muteBtn.lastElementChild.classList.toggle("hide");
 
-function playPauseAudio() {
-  playPauseBtn.firstElementChild.classList.toggle("hide");
-  playPauseBtn.lastElementChild.classList.toggle("hide");
+      audio.muted = !audio.muted;
 
-  if (audio.paused) {
-    audio.play();
-    stopBtn.disabled = false;
+      if (audio.muted) volumeControl.value = 0;
+      else volumeControl.value = audio.volume;
+    });
   }
-  else audio.pause();
-}
+  volumeActions();
 
-function stopAudio() {
-  if (!audio.paused) {
-    playPauseBtn.firstElementChild.classList.remove("hide");
-    playPauseBtn.lastElementChild.classList.add("hide");
+  function playProgressActions() {
+    const progressBar = document.getElementById('progressBar');
+    const currentTime = document.getElementById('currentTime');
+    const durationTime = document.getElementById('durationTime');
 
-    audio.pause();
-    audio.currentTime = 0;
-    progressBar.value = 0;
+    function formatTime(timeInSeconds) {
+      const minutes = Math.floor(timeInSeconds / 60);
+      const seconds = Math.floor(timeInSeconds % 60);
+      return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    }
+    function updateProgressBar() {
+      progressBar.value = audio.currentTime;
+      currentTime.textContent = formatTime(Math.round(audio.currentTime));
+    
+      progressBar.max = audio.duration;
+      durationTime.textContent = formatTime(Math.round(audio.duration));
+    }
+    audio.addEventListener('timeupdate', updateProgressBar);
+
+    progressBar.addEventListener('change', () => audio.currentTime = progressBar.value);
+    progressBar.addEventListener('input', (e) => currentTime.textContent = formatTime(Math.round(e.target.value)));
   }
-  stopBtn.disabled = true;
+  playProgressActions();
 }
+mainActions();
 
-function previousAudio() {
+function controlFunctions() {
+  const stopBtn = document.getElementById('stop');
+  function playAudio() {
+    if (audio.paused) {
+      audio.play();
+      stopBtn.disabled = false;
+    }
+    else audio.pause();
+  }
 
+  function prevNext() {
+    const songArtist = document.getElementById('trackArtist');
+    const songTitle = document.getElementById('trackTitle');
+    const trackCover = document.getElementById('trackCover');
+
+    const previousBtn = document.getElementById('prev');
+    const nextBtn = document.getElementById('next');
+
+    let songIndex = 0;
+
+    const data = [
+      {
+        title: "Don't Hurt Yourself",
+        artist: 'Beyonce',
+        src: 'assets/audio/beyonce.mp3',
+        cover: './assets/img/lemonade.png'
+      },
+      {
+        title: "Don't Start Now",
+        artist: 'Dua Lipa',
+        src: 'assets/audio/dontstartnow.mp3',
+        cover: './assets/img/dontstartnow.png'
+      }
+    ];
+
+    function updateSingInfo() {
+      audio.src = data[songIndex].src;
+      songArtist.textContent = data[songIndex].artist;
+      songTitle.textContent = data[songIndex].title;
+      trackCover.src = data[songIndex].cover;
+      document.body.style.backgroundImage = `url('${data[songIndex].cover}')`;
+    }
+    updateSingInfo();
+
+
+    previousBtn.addEventListener('click', () => {
+      songIndex--;
+      if (songIndex <= 0) songIndex = data.length-1;
+      updateSingInfo();
+      playAudio();
+    });
+
+
+    function nextSong() {
+      songIndex++;
+      if (songIndex > data.length-1) songIndex = 0;
+      updateSingInfo();
+      playAudio();
+    }
+    nextBtn.addEventListener('click', nextSong);
+
+    audio.addEventListener('ended', nextSong);
+  }
+  prevNext();
+
+  function playPause() {
+    const playPauseBtn = document.getElementById('playPause');
+
+    function playPauseAudio() {
+      playPauseBtn.firstElementChild.classList.toggle("hide");
+      playPauseBtn.lastElementChild.classList.toggle("hide");
+      playAudio();
+    }
+    playPauseBtn.addEventListener('click', playPauseAudio);
+
+    stopBtn.addEventListener('click', () => {
+      if (!audio.paused) {
+        playPauseBtn.firstElementChild.classList.remove("hide");
+        playPauseBtn.lastElementChild.classList.add("hide");
+    
+        audio.pause();
+        audio.currentTime = 0;
+        progressBar.value = 0;
+      }
+      stopBtn.disabled = true;
+    });
+  }
+  playPause();
 }
-
-function nextAudio() {
-  playIndex++;
-  audio.src = playlist[playIndex];
-  songArtist.textContent = songArtists[playIndex];
-  songTitle.textContent = songTitles[playIndex];
-  playerBox.style.backgroundImage = `url('${thumbnails[playIndex]}')`;
-  playPauseAudio();
-}
-
-function changeProgressBar() {
-  audio.currentTime = progressBar.value;
-}
-
-function changeVolume() {
-  audio.volume = volumeControl.value;
-}
-
-function muteVolume() {
-  muteBtn.firstElementChild.classList.toggle("hide");
-  muteBtn.lastElementChild.classList.toggle("hide");
-
-  audio.muted = !audio.muted;
-
-  if (audio.muted) volumeControl.value = 0;
-  else volumeControl.value = audio.volume;
-}
+controlFunctions();
